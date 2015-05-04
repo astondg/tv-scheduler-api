e828f219bab3e8306b66eb21c88ecad234756bad
Aston <aston.gilliland@outlook.com>
2015-05-04 12:51:59 +1000

    Added static data to Channel & Programs controllers

--- api/controllers/ChannelController.js
+++ api/controllers/ChannelController.js
@@ -6,6 +6,96 @@
  */
 
 module.exports = {
-	
+
+    find: function (req, res) {
+        return res.send([
+            {
+                name: 'test program 1',
+                description: 'bla bla bla this is a description of a program that is showing at a time on a channel',
+                startTime: moment(),
+                endTime: moment().add(60, 'minutes'),
+                channelId: 1
+            },
+            {
+                name: 'test program 2',
+                description: '',
+                startTime: moment().add(60, 'minutes'),
+                endTime: moment().add(100, 'minutes'),
+                channelId: 1
+            },
+            {
+                name: 'test program 3',
+                description: '',
+                startTime: moment().add(100, 'minutes'),
+                endTime: moment().add(125, 'minutes'),
+                channelId: 1
+            },
+            {
+                name: 'test program 4',
+                description: '',
+                startTime: moment(),
+                endTime: moment().add(15, 'minutes'),
+                channelId: 2
+            },
+            {
+                name: 'test program 5',
+                description: '',
+                startTime: moment().add(15, 'minutes'),
+                endTime: moment().add(70, 'minutes'),
+                channelId: 2
+            },
+            {
+                name: 'test program 6',
+                description: '',
+                startTime: moment().add(70, 'minutes'),
+                endTime: moment().add(160, 'minutes'),
+                channelId: 2
+            },
+            {
+                name: 'test program 7',
+                description: '',
+                startTime: moment().add(-20, 'minutes'),
+                endTime: moment().add(100, 'minutes'),
+                channelId: 3
+            },
+            {
+                name: 'test program 8',
+                description: '',
+                startTime: moment().add(100, 'minutes'),
+                endTime: moment().add(180, 'minutes'),
+                channelId: 3
+            },
+
+            {
+                name: 'test program 9',
+                description: '',
+                startTime: moment(),
+                endTime: moment().add(30, 'minutes'),
+                channelId: 5
+            },
+            {
+                name: 'test program 10',
+                description: '',
+                startTime: moment().add(30, 'minutes'),
+                endTime: moment().add(60, 'minutes'),
+                channelId: 5
+            },
+            {
+                name: 'test program 11',
+                description: '',
+                startTime: moment().add(60, 'minutes'),
+                endTime: moment().add(100, 'minutes'),
+                channelId: 5
+            },
+            {
+                name: 'test program 12',
+                description: '',
+                startTime: moment().add(100, 'minutes'),
+                endTime: moment().add(120, 'minutes'),
+                channelId: 5
+            },
+        ]);
+    }
+
 };
 
--- api/controllers/ProgramController.js
+++ api/controllers/ProgramController.js
@@ -7,5 +7,45 @@
 
 module.exports = {
 	
+    find: function (req, res) {
+        res.send([
+            {
+                id: 1,
+                name: 'One HD',
+                number: 1,
+                iconPath: 'img/250px-Channel_seven.jpg',
+                tunerId: '111AAA'
+            },
+            {
+                id: 2,
+                name: 'ABC',
+                number: 2,
+                iconPath: 'img/250px-Channel_seven.jpg',
+                tunerId: '222BBB'
+            },
+            {
+                id: 3,
+                name: 'SBSONE',
+                number: 3,
+                iconPath: 'img/250px-Channel_seven.jpg',
+                tunerId: '333CCC'
+            },
+            {
+                id: 4,
+                name: 'Seven',
+                number: 7,
+                iconPath: 'img/250px-Channel_seven.jpg',
+                tunerId: '444DDD'
+            },
+            {
+                id: 5,
+                name: 'Nine',
+                number: 9,
+                iconPath: 'img/250px-Channel_seven.jpg',
+                tunerId: '555EEE'
+            }
+        ]);
+    }
+
 };
 
